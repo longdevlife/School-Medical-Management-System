b7b899067715dfb9ab1b385d21ae1756d653ceb8
nguyenvy1904 <nguyenvy19042005@gmail.com>
2025-07-02 14:10:38 +0700

    up file

--- Sever/Sever/obj/Debug/net9.0/Sever.AssemblyInfo.cs
+++ Sever/Sever/obj/Debug/net9.0/Sever.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sever")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2086a7db8953b7c152132d4ba757ee7d52f200da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7753f87cc045404334d3c8cc5e530e87a7b91c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sever")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sever")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

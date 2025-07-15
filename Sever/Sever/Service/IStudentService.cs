30a2bfbb07ed06af7d8a8895296d8153d54e0a1e
Truong <satruong0@gmail.com>
2025-07-15 08:02:40 +0700

    Fix SM1-I3 - Hoàn tất xử lý lỗi tải danh sách học sinh"

--- Sever/Sever/Service/IStudentService.cs
+++ Sever/Sever/Service/IStudentService.cs
@@ -66,10 +66,7 @@ namespace Sever.Service
             try
             {
                 var studentProfiles = await _studentProfileRepository.GetStudentProfileByParentId(parentID);
-                if(studentProfiles != null)
-                {
-                    return null;
-                }
+              
                 foreach (var student in studentProfiles)
                 {
                     studentInfoList.Add(new GetStudentInfoRequest

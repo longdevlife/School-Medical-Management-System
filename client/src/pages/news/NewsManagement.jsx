544b16222e3195e197ca4fd14427f51d7373a925
longdevlife <hoangtira@gmail.com>
2025-07-29 02:37:27 +0700

    update deploy

--- client/src/pages/news/NewsManagement.jsx
+++ client/src/pages/news/NewsManagement.jsx
@@ -234,7 +234,7 @@ function NewsManagement() {
 
   const handleDelete = async (newsId) => {
     try {
-      // Nếu newsId là object hoặc có trường khác, lấy đúng trường id thực tế (newsID)
+      // Nếu newsId là object hoặc có trường khác, lấy đúng trường id thực tế (newID)
       const realId =
         typeof newsId === "object" && newsId.newsID ? newsId.newsID : newsId;
       const baseURL =
